6d67e962bf669d385daa8f2f04d99798ec327321
Angelogama21 <ang21gm33@gmail,com>
2024-02-20 22:30:35 -0600

    Prueba de actualización en Vercel

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='bg-green-500'>
-      <h1 className='text-6xl'>HomePage</h1>
+      <h1 className='text-6xl'>Hola</h1>
       <span>HomePage</span>
     </div>
   )
